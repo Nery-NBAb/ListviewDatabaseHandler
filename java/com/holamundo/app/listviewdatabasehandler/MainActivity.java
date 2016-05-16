7892547b8a4ebdedc48532baae92e8b0d3cb48e1
Nery-NBAb <neryyren11@gmail.com>
2016-05-16 12:21:41 -0600

    notificación agregada
    notificación agregada

--- java/com/holamundo/app/listviewdatabasehandler/MainActivity.java
+++ java/com/holamundo/app/listviewdatabasehandler/MainActivity.java
@@ -1,37 +1,60 @@
 package com.holamundo.app.listviewdatabasehandler;
 
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private static final int NOTIFICATION_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView estaConectado = (TextView) findViewById(R.id.textView);
+        if (estaConectado())
+            estaConectado.setText("Esta Conectado");
+        else
+            estaConectado.setText("No Esta Conectado");
+        mostrarNotificacion();
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void mostrarNotificacion(){
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("www.google.com"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationManager manager;
+        NotificationCompat.Builder builder;
+        builder = new NotificationCompat.Builder(this);
+
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(true);
+        builder.setContentTitle("Primera Notificacion");
+        builder.setContentText("ES la primera notificación");
+        builder.setSubText("Favor atender. Urgente. ");
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(
+                NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+    private boolean estaConectado() {
+        ConnectivityManager connManager = (ConnectivityManager)getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
             return true;
-        }
+        else
+            return false;
 
-        return super.onOptionsItemSelected(item);
     }
 }
